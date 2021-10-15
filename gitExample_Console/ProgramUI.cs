51ebe76db177fb09eabe8aedba00390c45bc2b69
Martin Flaherty <flaherty.martin@gmail.com>
2021-10-15 09:53:07 -0400

    Add project files.

---
+++ gitExample_Console/ProgramUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitExample_Console
+{
+    public class ProgramUI
+    {
+        public void Run()
+        {
+            // Seed data
+            // Main menu call
+            // Any post run cleanup
+        }
+    }
+}
