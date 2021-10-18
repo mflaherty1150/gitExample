38224f5f4b9095b5cad005faaa52ce4b8eea9b6d
Martin Flaherty <flaherty.martin@gmail.com>
2021-10-18 18:50:37 -0400

    Conflict start

--- gitExample_Console/ProgramUI.cs
+++ gitExample_Console/ProgramUI.cs
@@ -13,6 +13,9 @@ namespace gitExample_Console
             // Seed data
             // Main menu call
             // Any post run cleanup
+
+            // Merge conflict attempt
+            // More blather
         }
 
         public void Menu()
